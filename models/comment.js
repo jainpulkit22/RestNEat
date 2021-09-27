19ee21220e1b1694d4afe77fd6171056b7c77fb2
jainpulkit22 <18ucs163@lnmiit.ac.in>
2021-09-27 19:48:36 +0530

    Backend Changes Done

--- models/comment.js
+++ models/comment.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose")
 var commschema = new mongoose.Schema({
     text: String,
+    rating: Number,
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId,
